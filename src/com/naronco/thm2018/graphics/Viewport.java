aa7d36759a9148d01237d1cedc72eaf29b97945a
WebFreak001 <gh@webfreak.org>
2018-10-06 01:40:44 +0200

    Add LOD render method & refactor projection into separate method

--- src/com/naronco/thm2018/graphics/Viewport.java
+++ src/com/naronco/thm2018/graphics/Viewport.java
@@ -18,27 +18,52 @@ public class Viewport {
 		this.cameraPosition = new Vector2d(0, 0);
 	}
 
+	public static Vector2d projectWorldToViewport(double width, double height, Vector2d input, double rotation) {
+		double x0 = width * 0.5;
+		double y0 = height * 1.0 / HORIZON_OFFSET;
+
+		double sin = Math.sin(rotation);
+		double cos = Math.cos(rotation);
+
+		double xs = input.getX() * cos + input.getY() * -sin;
+		double ys = input.getX() * sin + input.getY() * cos;
+
+		double theta = CAMERA_HEIGHT / ys;
+		double phi = xs / ys;
+
+		double xp = phi * width + x0;
+		double yp = theta * height + y0;
+
+		return new Vector2d(xp, yp);
+	}
+
+	public Vector2d projectWorldToViewport(Screen screen, double x, double y) {
+		return projectWorldToViewport(screen.getDimension().getWidth(), screen.getDimension().getHeight(), new Vector2d(x, y), rotation);
+	}
+
+	public void renderSpriteLOD(Screen screen, double x, double y, double scale, int width, int height, Sprite sprite, int offsetX, int offsetY) {
+		if (y > 2.5 * scale) {
+			double y1 = projectWorldToViewport(screen, x, y - 2.5 * scale).getY();
+			double y2 = projectWorldToViewport(screen, x, y + 2.5 * scale).getY();
+
+			int factor = (int) Math.round(height / (y1 - y2));
+			width /= factor;
+			height /= factor;
+			offsetX /= factor;
+			offsetY /= factor;
+		}
+
+		renderSprite3D(screen, x, y, width, 0, width, height, sprite, offsetX, offsetY);
+	}
+
 	public void renderSprite3D(Screen screen, double x, double y, int startX, int startY, int width, int height, Sprite sprite) {
 		renderSprite3D(screen, x, y, startX, startY, width, height, sprite, 0, 0);
 	}
 
 	public void renderSprite3D(Screen screen, double x, double y, int startX, int startY, int width, int height, Sprite sprite, int offsetX, int offsetY) {
-		double x0 = screen.getDimension().getWidth() * 0.5;
-		double y0 = screen.getDimension().getHeight() * 1.0 / HORIZON_OFFSET;
+		Vector2d projected = projectWorldToViewport(screen, x, y);
 
-		double sin = Math.sin(rotation);
-		double cos = Math.cos(rotation);
-
-		double xs = x * cos + y * -sin;
-		double ys = x * sin + y * cos;
-
-		double theta = CAMERA_HEIGHT / ys;
-		double phi = xs / ys;
-
-		double xp = phi * screen.getDimension().getWidth() + x0;
-		double yp = theta * screen.getDimension().getHeight() + y0;
-
-		screen.renderSprite((int) (xp - width / 2) + offsetX, (int) (yp - height) + offsetY, startX, startY, width, height, sprite);
+		screen.renderSprite((int) (projected.getX() - width / 2) + offsetX, (int) (projected.getY() - height) + offsetY, startX, startY, width, height, sprite);
 	}
 
 	public void render(Screen screen) {
