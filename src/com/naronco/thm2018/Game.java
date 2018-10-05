9c6577d30c349f509105eca5c7bc40ba476f2c7f
Johannes Roth <johannes.roth18199@gmail.com>
2018-10-06 01:47:04 +0200

    drifting

--- src/com/naronco/thm2018/Game.java
+++ src/com/naronco/thm2018/Game.java
@@ -3,6 +3,7 @@ package com.naronco.thm2018;
 import com.deviotion.ld.eggine.Eggine;
 import com.deviotion.ld.eggine.graphics.*;
 import com.deviotion.ld.eggine.math.Dimension2d;
+import com.deviotion.ld.eggine.math.Vector2d;
 import com.naronco.thm2018.graphics.IViewportDataSource;
 import com.naronco.thm2018.graphics.Viewport;
 
@@ -11,8 +12,15 @@ import java.awt.event.KeyEvent;
 public class Game extends Eggine implements IViewportDataSource {
 	private Viewport viewport;
 
+	private Vector2d carPos = new Vector2d(0, 0);
+
 	private double crossingX = 0;
 	private double crossingY = 64;
+
+	private static final double TURN_TIME = 4.0;
+
+	private boolean animatingTurn = false;
+	private double animatingTurnTime = 0;
 
 	public Game() {
 		super(60, 60, new Window("InfinityJam", new Dimension2d(160, 120), 4));
@@ -28,16 +36,39 @@ public class Game extends Eggine implements IViewportDataSource {
 		screen.fillScreen(0xF2F2F2);
 
 		viewport.render(screen);
-		viewport.renderSprite3D(screen, 0, 7, 94, 0, 60, 61, Sprites.car);
+		viewport.renderSprite3D(screen, carPos.getX() - viewport.getCameraPosition().getX(), carPos.getY() - viewport.getCameraPosition().getY(), 0, 0, 94, 61, Sprites.car);
 	}
 
 	@Override
 	public void update(double delta) {
 		time += delta;
-		double y = viewport.getCameraPosition().getY();
-		if (y < crossingY - 16) {
-			viewport.getCameraPosition().setY(y + delta * 50 / 3.6);
+		if (carPos.getY() < crossingY - 9) {
+			carPos.setY(carPos.getY() + delta * 50 / 3.6);
+		} else {
+			if (!animatingTurn) {
+				animatingTurn = true;
+				animatingTurnTime = 0;
+			}
 		}
+
+		if (animatingTurn) {
+			animatingTurnTime += delta;
+			double p = animatingTurnTime / TURN_TIME;
+			if (p >= 1.0) {
+				p = 1.0;
+				animatingTurn = false;
+			}
+
+			double angle = p * Math.PI * 0.5;
+
+			carPos.setX(-9 + Math.cos(angle) * 9);
+			carPos.setY(crossingY - 9 + Math.sin(angle) * 9);
+
+			viewport.setRotation(-angle);
+		}
+
+		Vector2d camPos = carPos.add(new Vector2d(Math.sin(-viewport.getRotation()), -Math.cos(-viewport.getRotation())).multiply(7));
+		viewport.setCameraPosition(camPos);
 	}
 
 	@Override
